4aa3d114aa7f7735fcd21531340fe3d0d2aadfb5
Will Smith <will@toxon.co.uk>
2017-11-25 16:21:50 +0000

    Added Option<T> type

---
+++ SimpleDatabase.Options/Option.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SimpleDatabase.Options
+{
+    public struct Option<T>
+    {
+        public bool HasValue { get; }
+        public T Value { get; }
+
+        public Option(T value) { HasValue = true; Value = value; }
+
+        public Option<TOut> Select<TOut>(Func<T, TOut> fn)
+        {
+            return HasValue ? new Option<TOut>(fn(Value)) : new Option<TOut>();
+        }
+        public TOut Map<TOut>(Func<T, TOut> some, Func<TOut> none)
+        {
+            return HasValue ? some(Value) : none();
+        }
+    }
+
+    public static class Option
+    {
+        public static Option<T> Some<T>(T value)
+        {
+            return new Option<T>(value);
+        }
+
+        public static Option<T> None<T>()
+        {
+            return new Option<T>();
+        }
+    }
+
+}
---
+++ SimpleDatabase.Options/OptionExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleDatabase.Options
+{
+    public static class OptionExtensions
+    {
+        public static Option<T> ToOption<T>(this T value)
+            where T : class
+        {
+            return value != default(T) 
+                ? Option.Some(value) 
+                : Option.None<T>();
+        }
+
+
+        public static T OrElse<T>(this Option<T> opt, Func<T> func)
+        {
+            return opt.Map(x => x, func);
+        }
+
+        public static Option<T> Flatten<T>(this Option<Option<T>> opt)
+        {
+            return opt.Select(x => x.Value);
+        }
+        public static Option<TOut> Flatten<TIn, TOut>(this Option<TIn> opt, Func<TIn, Option<TOut>> some)
+        {
+            return opt.Select(some).Select(x => x.Value);
+        }
+
+        public static Option<TCast> As<TValue, TCast>(this Option<TValue> opt)
+            where TCast : class
+        {
+            return opt.Flatten(x => (x as TCast).ToOption());
+        }
+
+        public static Option<TValue> TryGet<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dict, TKey key)
+        {
+            return dict.TryGetValue(key, out var value)
+                ? Option.Some(value)
+                : Option.None<TValue>();
+        }
+    }
+
+}
