bf0eb9d07b8975449feab358cf73ebe94f8ff65c
Will Smith <will@toxon.co.uk>
2017-12-22 23:49:54 +0000

    Implement HeapTraverser

--- SimpleDatabase.Execution/Tables/HeapTraverser.cs
+++ SimpleDatabase.Execution/Tables/HeapTraverser.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using SimpleDatabase.Schemas;
+﻿using SimpleDatabase.Schemas;
+using SimpleDatabase.Storage.Heap;
 using SimpleDatabase.Storage.Paging;
 
 namespace SimpleDatabase.Execution.Tables
@@ -14,15 +14,37 @@ namespace SimpleDatabase.Execution.Tables
             _pager = pager;
             _table = table;
         }
-        
+
         public Cursor StartCursor()
         {
-            throw new NotImplementedException();
+            var pageId = new PageId(_pager.Source, 0);
+            var page = HeapPage.Read(_pager.Get(0));
+
+            var tableIsEmpty = page.ItemCount == 0;
+            var hasNextPage = page.NextPageIndex != 0;
+
+            return new Cursor(pageId, 0, tableIsEmpty && !hasNextPage);
         }
 
         public Cursor AdvanceCursor(Cursor cursor)
         {
-            throw new NotImplementedException();
+            var pageId = cursor.Page;
+            var cell = cursor.CellNumber + 1;
+
+            var page = HeapPage.Read(_pager.Get(pageId.Index));
+
+            if (cell >= page.ItemCount)
+            {
+                pageId = new PageId(pageId.Source, page.NextPageIndex);
+                cell = 0;
+
+                page = HeapPage.Read(_pager.Get(pageId.Index));
+            }
+            
+            var itemIsLast = cell + 1 == page.ItemCount;
+            var hasNextPage = page.NextPageIndex != 0;
+
+            return new Cursor(pageId, cell, itemIsLast && !hasNextPage);
         }
     }
 }
