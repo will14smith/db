4b5527ce301b11c9f174f8c14a5572d221929bb7
Will Smith <will@toxon.co.uk>
2018-01-14 13:40:58 +0000

    Added test to YieldOperationExecutor

---
+++ SimpleDatabase.Execution.UnitTests/OperationExecutors/YieldOperationExecutorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SimpleDatabase.Execution.OperationExecutors;
+using SimpleDatabase.Execution.Operations;
+using Xunit;
+
+namespace SimpleDatabase.Execution.UnitTests.OperationExecutors
+{
+    public class YieldOperationExecutorTests : OperationExecutorTestBase
+    {
+        [Fact]
+        public void State_ShouldPopSingleItem()
+        {
+            var (resultState, _) = new YieldOperationExecutor().Execute(BlankState.PushValue(RandomValue), new YieldOperation());
+
+            AssertEqualState(BlankState, resultState);
+        }
+
+        [Fact]
+        public void Result_ShouldYieldTheItemFromTheStack()
+        {
+            var value = RandomValue;
+
+            var (_, result) = new YieldOperationExecutor().Execute(BlankState.PushValue(value), new YieldOperation());
+
+            AssertEqualResult(new OperationResult.Yield(new OperationResult.Next(), value), result);
+        }
+
+        [Fact]
+        public void WhenStackIsEmpty_ShouldThrow()
+        {
+            Assert.Throws<InvalidOperationException>(() => new YieldOperationExecutor().Execute(BlankState, new YieldOperation()));
+        }
+    }
+}
