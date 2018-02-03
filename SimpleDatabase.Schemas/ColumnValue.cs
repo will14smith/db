6be4b9d0fa6fa93f36449ed1cff943d6b8174995
Will Smith <will@toxon.co.uk>
2018-02-03 19:55:00 +0000

    Fixed equality of ColumnValue

--- SimpleDatabase.Schemas/ColumnValue.cs
+++ SimpleDatabase.Schemas/ColumnValue.cs
@@ -1,6 +1,8 @@
-﻿namespace SimpleDatabase.Schemas
+﻿using System;
+
+namespace SimpleDatabase.Schemas
 {
-    public class ColumnValue
+    public class ColumnValue : IEquatable<ColumnValue>
     {
         public object Value { get; }
 
@@ -12,6 +14,34 @@
         public override string ToString()
         {
             return Value?.ToString();
+        }
+
+        public bool Equals(ColumnValue other)
+        {
+            if (other is null) return false;
+            return ReferenceEquals(this, other) || Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj is ColumnValue value && Equals(value);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value != null ? Value.GetHashCode() : 0;
+        }
+
+        public static bool operator ==(ColumnValue left, ColumnValue right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ColumnValue left, ColumnValue right)
+        {
+            return !Equals(left, right);
         }
     }
 }
